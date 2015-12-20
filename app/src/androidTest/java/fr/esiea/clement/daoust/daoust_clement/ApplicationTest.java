c0ef8df31325ebfdd8bafe46ace4d952b60c7039
vincent <vclement@et.esiea.fr>
2015-12-21 00:04:03 +0100

    Initial commit

---
+++ app/src/androidTest/java/fr/esiea/clement/daoust/daoust_clement/ApplicationTest.java
@@ -0,0 +1,13 @@
+package fr.esiea.clement.daoust.daoust_clement;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
